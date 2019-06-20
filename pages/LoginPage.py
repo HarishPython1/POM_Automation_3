1cd331fce23c44184b0845cf6b6abd564a14122e
harishpython1 <harish.python.com@gmail.com>
2019-06-20 09:15:26 +0530

    Added WebGeneric File

--- pages/LoginPage.py
+++ pages/LoginPage.py
@@ -1,9 +1,24 @@
-from selenium import  webdriver
-class Login:
+from pages.WebGeneric import WebGeneric
+class Login(WebGeneric):
     def __init__(self,driver):
+        WebGeneric.__init__(self,driver)
         self.driver=driver
+        self.un_id="username"
+        self.pwd_name="pwd"
+        self.login_btn_xpath="//*[text()='Login ']"
     def acti_login(self,un,pwd):
         # Login
-        self.driver.find_element_by_id("username").send_keys(un)
-        self.driver.find_element_by_name("pwd").send_keys(pwd)
-        self.driver.find_element_by_xpath("//*[text()='Login ']").click()
+        #wg=WebGeneric()
+        #wg=WebGeneric(driver)
+
+        # WebGeneric.enter(self.un_id,un)
+        # WebGeneric.enter(self.pwd_name,pwd)
+        # WebGeneric.submit(self.login_btn_xpath)
+        wg=WebGeneric(self.driver)
+        wg.enter(self.un_id,un)
+        wg.enter(self.pwd_name,pwd)
+        wg.submit(self.login_btn_xpath)
+
+        #self.driver.find_element_by_id(self.un_id).send_keys(un)
+        #self.driver.find_element_by_name(self.pwd_name).send_keys(pwd)
+        #self.driver.find_element_by_xpath(self.login_btn_xpath).click()
